0ac6511bba427baa38b158d02017486cf04c73a6
Armaan Khan <48325615+AkP2809@users.noreply.github.com>
2022-12-29 19:36:33 +0530

    Commit Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PasswordChecker } from './custom-validators/password-checker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'signup-reactive';
+
+  registerForm : FormGroup = this.formBuilder.group({
+    firstName: ["", Validators.required],
+    lastName: ["", Validators.required],
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ["", [Validators.required, Validators.minLength(8)]],
+    acceptTandC: [false, Validators.requiredTrue]
+  }, {
+    validators: PasswordChecker('password', 'confirmPassword')
+  } as AbstractControlOptions);
+
+  isSubmitted : boolean = false;
+
+  constructor(private formBuilder : FormBuilder) {}
+
+  //a simple get helper
+  get h() {
+    return this.registerForm.controls;
+  }
+
+  submitForm = () => {
+    this.isSubmitted = true;
+
+    if(this.registerForm.invalid) {
+      //Form is invalid
+      return;
+    }
+
+    console.table(this.registerForm.value);
+    console.table(this.registerForm);
+
+    alert("Success Signup!" + JSON.stringify(this.registerForm.value));
+  }
+
+  resetForm = () => {
+    this.isSubmitted = false;
+    this.registerForm.reset();
+  }
 }
